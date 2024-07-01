9f1ca4a21a9b72f18cc8ab7e46d10d17c1f3e6f4
Gwenhael Goavec-Merou <gwenhael@enjoy-digital.fr>
2024-07-01 14:28:55 +0200

    added intel_agilex5e_065b platform/target

---
+++ intel_agilex5e_065b_premium_devkit.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python3
+
+#
+# This file is part of LiteX-Boards.
+#
+# Copyright (c) 2024 Enjoy-Digital <enjoy-digital.fr>
+#
+# SPDX-License-Identifier: BSD-2-Clause
+
+from migen import *
+from migen.genlib.resetsync import AsyncResetSynchronizer
+
+from litex.gen import *
+
+from intel_agilex5e_065b_premium_devkit_platform import Platform
+
+from litex.soc.integration.soc_core import *
+from litex.soc.integration.builder import *
+from litex.soc.cores.led import LedChaser
+
+# CRG ----------------------------------------------------------------------------------------------
+
+class _CRG(LiteXModule):
+    def __init__(self, platform, sys_clk_freq):
+        self.rst    = Signal()
+        self.cd_sys = ClockDomain()
+        self.cd_por = ClockDomain()
+
+        # # #
+
+        # Clk / Rst
+        clk100 = platform.request("clk100")
+        rst_n  = platform.request("user_btn", 0)
+
+        # Clocking
+        self.comb += self.cd_sys.clk.eq(clk100)
+        platform.add_period_constraint(self.cd_sys.clk, 1e9/sys_clk_freq)
+
+        # Power on reset
+        por_count = Signal(16, reset=2**16-1)
+        por_done  = Signal()
+        self.comb += por_done.eq(por_count == 0)
+        self.comb += self.cd_por.clk.eq(self.cd_sys.clk)
+        self.sync.por += If(~por_done, por_count.eq(por_count - 1))
+        self.specials += [
+            AsyncResetSynchronizer(self.cd_por, ~rst_n),
+            AsyncResetSynchronizer(self.cd_sys, ~por_done | self.rst)
+        ]
+
+# BaseSoC ------------------------------------------------------------------------------------------
+
+class BaseSoC(SoCCore):
+    def __init__(self, sys_clk_freq=100e6, with_led_chaser=True, **kwargs):
+        platform = Platform()
+
+        # CRG --------------------------------------------------------------------------------------
+        self.crg = _CRG(platform, sys_clk_freq)
+
+        # SoCCore ----------------------------------------------------------------------------------
+        SoCCore.__init__(self, platform, sys_clk_freq, ident="LiteX SoC on Agilex5E 065B", **kwargs)
+
+        # Leds -------------------------------------------------------------------------------------
+        if with_led_chaser:
+            self.leds = LedChaser(
+                pads         = platform.request_all("user_led"),
+                sys_clk_freq = sys_clk_freq)
+
+# Build --------------------------------------------------------------------------------------------
+
+def main():
+    from litex.build.parser import LiteXArgumentParser
+    parser = LiteXArgumentParser(platform=Platform, description="LiteX SoC on LiteX SoC on Agilex5E 065B.")
+    parser.add_target_argument("--sys-clk-freq", default=100e6, type=float, help="System clock frequency.")
+    args = parser.parse_args()
+
+    soc = BaseSoC(
+        sys_clk_freq = args.sys_clk_freq,
+        **parser.soc_argdict
+    )
+
+    builder = Builder(soc, **parser.builder_argdict)
+    if args.build:
+        builder.build(**parser.toolchain_argdict)
+
+    if args.load:
+        prog = soc.platform.create_programmer()
+        prog.load_bitstream(builder.get_bitstream_filename(mode="sram"))
+
+if __name__ == "__main__":
+    main()
+
---
+++ intel_agilex5e_065b_premium_devkit_platform.py
@@ -0,0 +1,102 @@
+#
+# This file is part of LiteX-Boards.
+#
+# Copyright (c) 2024 Enjoy-Digital <enjoy-digital.fr>
+#
+# SPDX-License-Identifier: BSD-2-Clause
+
+from litex.build.altera            import AlteraPlatform
+from litex.build.altera.programmer import USBBlaster
+from litex.build.generic_platform  import Pins, IOStandard, Subsignal, Misc
+
+# IOs ----------------------------------------------------------------------------------------------
+
+_io = [
+
+    # Clk
+    ("clk100",     0, Pins("D8"),    IOStandard("3.3-V LVCMOS")), # SI5332 OUT4
+    ("sys_clk100", 0, Pins("BF111"), IOStandard("3.3-V LVCMOS")), # SI5332 OUT5
+
+    # Serial
+    ("serial", 0,
+        Subsignal("rx", Pins("CJ2")),
+        Subsignal("tx", Pins("CK4")),
+        IOStandard("3.3-V LVCMOS"),
+    ),
+
+    # Leds
+    ("user_led", 0, Pins("BM59"), IOStandard("1.1 V")),
+    ("user_led", 0, Pins("BH59"), IOStandard("1.1 V")),
+    ("user_led", 0, Pins("BH62"), IOStandard("1.1 V")),
+    ("user_led", 0, Pins("BK59"), IOStandard("1.1 V")),
+
+    # Switches
+    ("user_sw", 0, Pins("CH12"), IOStandard("3.3-V LVCMOS")),
+    ("user_sw", 1, Pins("BU22"), IOStandard("3.3-V LVCMOS")),
+    ("user_sw", 2, Pins("BW19"), IOStandard("3.3-V LVCMOS")),
+    ("user_sw", 3, Pins("BH28"), IOStandard("3.3-V LVCMOS")),
+
+    # Buttons
+    ("user_btn", 0, Pins("BK31"), IOStandard("3.3-V LVCMOS")),
+    ("user_btn", 1, Pins("BP22"), IOStandard("3.3-V LVCMOS")),
+    ("user_btn", 2, Pins("BK28"), IOStandard("3.3-V LVCMOS")),
+    ("user_btn", 3, Pins("BR22"), IOStandard("3.3-V LVCMOS")),
+
+    # SFP
+
+    ("sfp", 0,
+        Subsignal("clk_p", Pins("BC29")),
+        Subsignal("clk_n", Pins("BC25")),
+        Subsignal("rx_p",  Pins("BN1")),
+        Subsignal("rx_n",  Pins("BN3")),
+        Subsignal("tx_p",  Pins("BL7")),
+        Subsignal("tx_n",  Pins("BL10")),
+    ),
+    ("sfp_rx", 0,
+        Subsignal("p",     Pins("BN1")),
+        Subsignal("n",     Pins("BN3")),
+    ),
+    ("sfp_tx", 0,
+        Subsignal("p",     Pins("BL7")),
+        Subsignal("n",     Pins("BL10")),
+    ),
+    ("sfp_tx_disable_n", 0, Pins("AU10")),
+    ("sfp0_ctl", 0,
+        Subsignal("prsntn",        Pins("BF32")),
+        Subsignal("tx_fault",      Pins("BE25")),
+        Subsignal("rx_los",        Pins("BE29")),
+        Subsignal("rs0",           Pins("BE21")),
+        Subsignal("rs1",           Pins("BE43")),
+        Subsignal("link_act_ledn", Pins("BR19")),
+        IOStandard("3.3-V LVCMOS"),
+    ),
+
+    ("qsfp", 0,
+        Subsignal("clk_p",  Pins("BB120")),
+        Subsignal("clk_n",  Pins("BB115")),
+        Subsignal("rx_p",   Pins("BV135 BN135 BJ135 BF135")),
+        Subsignal("rx_n",   Pins("BV133 BN133 BJ133 BF133")),
+        Subsignal("tx_p",   Pins("BY129 BT129 BL129 BG129")),
+        Subsignal("tx_n",   Pins("BY126 BT126 BL126 BG126")),
+    ),
+]
+
+# Connectors ---------------------------------------------------------------------------------------
+
+_connectors = [
+           # VCC                                         GND
+    ("j9", " --- BU31 BU28 BM28 BP31 BF21 BR28 BM31 BR31 ---"),
+]
+
+# Platform -----------------------------------------------------------------------------------------
+
+class Platform(AlteraPlatform):
+    default_clk_name   = "clk100"
+    default_clk_period = 1e9/100e6
+
+    def __init__(self, toolchain="quartus"):
+        AlteraPlatform.__init__(self, "", _io, _connectors, toolchain=toolchain)
+
+    def do_finalize(self, fragment):
+        AlteraPlatform.do_finalize(self, fragment)
+        self.add_period_constraint(self.lookup_request("clk100", loose=True), 1e9/100e6)
